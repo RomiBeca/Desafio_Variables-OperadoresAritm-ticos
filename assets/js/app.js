45d5f9c85c4df25a400e56b452cfdcb4df7beb39
Romina Beca <rominabeca.v@gmail.com>
2024-02-27 13:16:06 -0300

    operaciones-listas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,48 @@
-console.log("esto es una prueba ¡si funciono! ")
+// console.log("esto es una prueba ¡si funciono! ")
 // diagrama
 
 // operaciones matematicas
+
+function sumar() {
+    let numeroUno = parseFloat(document.getElementById("numero1").value);
+    let numeroDos = parseFloat(document.getElementById("numero2").value);
+
+    let suma = numeroUno + numeroDos;
+
+    document.getElementById("resultado1").value = suma;
+}
+function resta() {
+    let numeroTres = parseFloat(document.getElementById("numero3").value);
+    let numeroCuatro = parseFloat(document.getElementById("numero4").value);
+
+    let resta = numeroTres - numeroCuatro;
+
+    document.getElementById("resultado2").value = resta;
+}
+function multiplicacion() {
+    let numeroCinco = parseFloat(document.getElementById("numero5").value);
+    let numeroSeis = parseFloat(document.getElementById("numero6").value);
+
+    let multiplicacion = numeroCinco * numeroSeis
+
+    document.getElementById("resultado3").value = multiplicacion;
+}
+function division() {
+    let numeroSiete = parseFloat(document.getElementById("numero7").value);
+    let numeroOcho = parseFloat(document.getElementById("numero8").value);
+
+    let division = numeroSiete / numeroOcho
+
+    document.getElementById("resultado4").value = division;
+}
+function modulo() {
+    let numeroNueve = parseFloat(document.getElementById("numero9").value);
+    let numeroDiez = parseFloat(document.getElementById("numero10").value);
+
+    let modulo = numeroNueve % numeroDiez
+
+    document.getElementById("resultado5").value = modulo;
+}
 
 //temperaturas
 
